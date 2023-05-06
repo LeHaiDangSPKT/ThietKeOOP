e629930e99114a420e122a82f86f5042d0ce66a5
LuongXuanThang20110724 <20110724@student.hcmute.edu.vn>
2023-05-07 04:07:18 +0700

    complete ver1 (without StateDiagram)

---
+++ Demo/22/demoTemplateMethod/demoTemplateMethod/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace demoTemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PDFDataMiner pdfDataMiner = new PDFDataMiner();
+            pdfDataMiner.mine("pdf path");
+
+            CSVDataMiner csvDataMiner = new CSVDataMiner();
+            csvDataMiner.mine("csv path");
+        }
+    }
+
+
+    public abstract class DataMiner
+    {
+        string file;
+        public string File { get => file; set => file = value; }
+
+        string rawData;
+        public string RawData { get => rawData; set => rawData = value; }
+
+        public virtual void openFile(string path)
+        {
+            Console.WriteLine("Open " + path);
+        }
+        public virtual void closeFile()
+        {
+            Console.WriteLine("Close file\n");
+        }
+        public abstract void extractData();
+        public abstract void parseData();
+
+        public void mine(string path)
+        {
+            openFile(path);
+            extractData();
+            parseData();
+            closeFile();
+        }
+
+    }
+
+
+
+    public class CSVDataMiner : DataMiner
+    {
+        public override void extractData()
+        {
+            Console.WriteLine("Extract CSV");
+        }
+
+        public override void parseData()
+        {
+            Console.WriteLine("Parse CSV");
+        }
+    }
+
+    public class PDFDataMiner : DataMiner
+    {
+        public override void openFile(string path)
+        {
+            base.openFile(path);
+            this.File = "PDF";
+        }
+        public override void extractData()
+        {
+            Console.WriteLine("Extract " + this.File);
+            this.RawData = "PDF";
+        }
+
+        public override void parseData()
+        {
+            Console.WriteLine("Parse " + this.RawData);
+        }
+    }
+}
